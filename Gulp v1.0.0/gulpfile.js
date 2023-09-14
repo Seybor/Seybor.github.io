4eae460547bfbb90304cf75351faab6b93d51404
Seybor <sashaborisenko1996@gmail.com>
2023-09-14 20:36:49 +0700

    Сборки для gulp

---
+++ Gulp v1.0.0/gulpfile.js
@@ -0,0 +1,69 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+
+function scripts() {
+	return src([
+		'app/js/main.js',
+		// ! Чтобы не было зацикливания, нужно прописать, что concat возьмёт все файлы js в папке app, кроме main.min.js
+		'!app/js/main.min.js'
+	])
+		.pipe(concat('main.min.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream());
+}
+
+function styles() {
+	return src('app/scss/style.scss')
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
+		.pipe(concat('style.min.css'))
+		.pipe(scss({ outputStyle: 'compressed' }).on('error', scss.logError))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream());
+
+}
+
+function watching() {
+	watch(['app/scss/style.scss'], styles)
+	watch(['app/js/main.js'], scripts)
+	watch(['app/*.html']).on('change', browserSync.reload)
+
+	// ! Если нужно, чтобы искало во всех файлах html, а не только в конкретной папке, то нужно прописать:
+	// ^ watch(['app/**/*.html'], scripts)
+}
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: "app/"
+		}
+	});
+}
+
+function cleanDist() {
+	return src('dist')
+		.pipe(clean())
+}
+
+function building() {
+	return src([
+		'app/css/style.min.css',
+		'app/js/main.min.js',
+		'app/**/*.html'
+	], { base: 'app' })
+		.pipe(dest('dist'))
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.build = series(cleanDist, building);
+exports.default = parallel(styles, scripts, browsersync, watching);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Seybor.github.io
 
+13.09.23 - Gulp v.1.0.0
+14.09.23 - Gulp v.1.5.0
+14.09.23 20:30 - Gulp V.1.5.5 - add plugin jquery; add fail .gitignore
