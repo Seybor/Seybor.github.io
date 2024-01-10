bf6834cdb1e35aa5e21030f11b9d69cc07d41ca6
Seybor <sashaborisenko1996@gmail.com>
2024-01-10 23:12:01 +0700

    f

--- ENGLISH-DICTIONARY/scripts/A1/Words-A1.js
+++ ENGLISH-DICTIONARY/scripts/A1/Words-A1.js
@@ -507,6 +507,7 @@ thead.addEventListener('click', (evt) => {
 })
 
  const shuffleArr = (arr) => {
+    enSort = false
     for (let i = arr.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       let t = arr[i];
