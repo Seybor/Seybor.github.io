d5a1d84419edefab26ea8cda259ac1db328f6589
Seybor <sashaborisenko1996@gmail.com>
2024-07-18 19:30:12 +0700

    v2.3.0

--- anki-deck-2/js/main.js
+++ anki-deck-2/js/main.js
@@ -2,282 +2,285 @@ import { s, all } from './modules/base.js'
 
 import ALL from './data/all.js'
 
-let sum = 0
+const startScript = () => {
+	let sum = 0
 
-Object.entries(ALL).forEach(e => {
-	sum += e[1].length
+	Object.entries(ALL).forEach(e => {
+		sum += e[1].length
 
-	if (e[1][0]?.pos) {
-		e[1].sort((a, b) => {
-			return a.pos - b.pos
+		if (e[1][0]?.pos) {
+			e[1].sort((a, b) => {
+				return a.pos - b.pos
+			})
+		}
+
+	})
+
+	console.log(ALL)
+
+	s('#arr-sum').textContent = sum
+
+	const getNum = () => {
+
+		function startNum(arr, wordsIntervalOne = 0, wordsIntervalTwo = 0) {
+			if (wordsIntervalOne > wordsIntervalTwo) {
+				alert('Ошибка в интервалах')
+				return
+			}
+			let i = arr.findIndex(num => num.pos > wordsIntervalOne)
+			let sum = 0
+			do {
+				sum++
+				if (i >= arr.length - 1) {
+					console.log(i)
+					break
+				}
+				i++
+			} while (arr[i].pos >= wordsIntervalOne && arr[i].pos < wordsIntervalTwo)
+
+			return sum
+		}
+
+		all('.nav__link span').forEach(e => {
+
+			if (ALL[e.parentElement.dataset.id]) {
+				e.textContent = ALL[e.parentElement.dataset.id].length
+			} else {
+				switch (e.parentElement.dataset.id) {
+					case 'w-1': {
+						e.textContent = startNum(ALL.w, 1, 1000)
+					} break
+					case 'w-2': {
+						e.textContent = startNum(ALL.w, 1000, 2000)
+					} break
+					case 'w-3': {
+						e.textContent = startNum(ALL.w, 2000, 3000)
+					} break
+					case 'w-4': {
+						e.textContent = startNum(ALL.w, 3000, 4000)
+					} break
+					case 'w-5': {
+						e.textContent = startNum(ALL.w, 4000, 5000)
+					} break
+					case 'w-6': {
+						e.textContent = startNum(ALL.w, 5000, 6000)
+					} break
+					case 'w-7': {
+						e.textContent = startNum(ALL.w, 6000, 10000)
+					} break
+					case 'w-8': {
+						e.textContent = startNum(ALL.w, 10000, 22000)
+					} break
+					case 'w-9': {
+						e.textContent = startNum(ALL.w, 21999, 22001)
+					} break
+				}
+			}
+
+
+
 		})
+
+
+
+
+
 	}
 
-})
-
-console.log(ALL)
-
-s('#arr-sum').textContent = sum
-
-const getNum = () => {
-
-	function startNum(arr, wordsIntervalOne = 0, wordsIntervalTwo = 0) {
-		if (wordsIntervalOne > wordsIntervalTwo) {
-			alert('Ошибка в интервалах')
-			return
-		}
-		let i = arr.findIndex(num => num.pos > wordsIntervalOne)
-		let sum = 0
-		do {
-			sum++
-			if (i >= arr.length - 1) {
-				console.log(i)
-				break
+	const getSearch = () => {
+		s('.search').addEventListener('click', (evt) => {
+			if (evt.target === evt.currentTarget) {
+				s('.search').classList.toggle('search--active')
 			}
-			i++
-		} while (arr[i].pos >= wordsIntervalOne && arr[i].pos < wordsIntervalTwo)
-
-		return sum
-	}
-
-	all('.nav__link span').forEach(e => {
-
-		if (ALL[e.parentElement.dataset.id]) {
-			e.textContent = ALL[e.parentElement.dataset.id].length
-		} else {
-			switch (e.parentElement.dataset.id) {
-				case 'w-1': {
-					e.textContent = startNum(ALL.w, 1, 1000)
-				} break
-				case 'w-2': {
-					e.textContent = startNum(ALL.w, 1000, 2000)
-				} break
-				case 'w-3': {
-					e.textContent = startNum(ALL.w, 2000, 3000)
-				} break
-				case 'w-4': {
-					e.textContent = startNum(ALL.w, 3000, 4000)
-				} break
-				case 'w-5': {
-					e.textContent = startNum(ALL.w, 4000, 5000)
-				} break
-				case 'w-6': {
-					e.textContent = startNum(ALL.w, 5000, 6000)
-				} break
-				case 'w-7': {
-					e.textContent = startNum(ALL.w, 6000, 10000)
-				} break
-				case 'w-8': {
-					e.textContent = startNum(ALL.w, 10000, 22000)
-				} break
-				case 'w-9': {
-					e.textContent = startNum(ALL.w, 21999, 22001)
-				} break
+		})
+		s('.search__btn').addEventListener('click', (evt) => {
+			searchStart()
+		})
+		s('.search__input').addEventListener('keydown', (evt) => {
+			if (evt.key === 'Enter' || evt.keyCode === 13) {
+				s('.search__btn').click()
 			}
-		}
-
-
-
-	})
-
-
-
-
-
-}
-
-const getSearch = () => {
-	s('.search').addEventListener('click', (evt) => {
-		if (evt.target === evt.currentTarget) {
+		})
+		s('#search').addEventListener('click', (evt) => {
 			s('.search').classList.toggle('search--active')
-		}
-	})
-	s('.search__btn').addEventListener('click', (evt) => {
-		searchStart()
-	})
-	s('.search__input').addEventListener('keydown', (evt) => {
-		if (evt.key === 'Enter' || evt.keyCode === 13) {
-			s('.search__btn').click()
-		}
-	})
-	s('#search').addEventListener('click', (evt) => {
-		s('.search').classList.toggle('search--active')
-		s('.search__input').focus()
-	})
+			s('.search__input').focus()
+		})
 
 
-	function searchStart() {
+		function searchStart() {
 
-		let str = s('.search__input').value.trim().toLowerCase()
+			let str = s('.search__input').value.trim().toLowerCase()
 
-		if (str <= 1) {
-			s('.search__input').value = ''
-			s('.search__input').placeholder = `длина строки слишком маленькая`
-			return
-		}
-
-		let ruEnStr = isFirstLetterEnglish(str)
-		let ruOrEn = ''
-
-		if (!ruEnStr) {
-			ruEnStr = isFirstLetterRussian(str)
-
-			if (!ruEnStr) {
+			if (str <= 1) {
 				s('.search__input').value = ''
-				s('.search__input').placeholder = `недопустимые символы`
+				s('.search__input').placeholder = `длина строки слишком маленькая`
 				return
 			}
 
-			ruOrEn = 'ru'
+			let ruEnStr = isFirstLetterEnglish(str)
+			let ruOrEn = ''
 
-		} else {
-			ruOrEn = 'en'
-		}
+			if (!ruEnStr) {
+				ruEnStr = isFirstLetterRussian(str)
+
+				if (!ruEnStr) {
+					s('.search__input').value = ''
+					s('.search__input').placeholder = `недопустимые символы`
+					return
+				}
+
+				ruOrEn = 'ru'
+
+			} else {
+				ruOrEn = 'en'
+			}
 
 
-		let result = []
+			let result = []
 
-		Object.entries(ALL).forEach(e => {
-			let el = e[1].filter(item => {
-				return item[ruOrEn].includes(str)
+			Object.entries(ALL).forEach(e => {
+				let el = e[1].filter(item => {
+					return item[ruOrEn].includes(str)
+				})
+
+				result = result.concat(el)
+
 			})
 
-			result = result.concat(el)
+			console.log(result.length)
 
-		})
+			if (result.length <= 0) {
+				s('.wrap').textContent = ''
+				s('.search__input').value = ''
+				s('.search__input').placeholder = `найдено: 0 совпадений`
+			} else {
+				renderStart(result)
+			}
 
-		console.log(result.length)
 
-		if (result.length <= 0) {
+		}
+
+		function renderStart(result) {
 			s('.wrap').textContent = ''
 			s('.search__input').value = ''
-			s('.search__input').placeholder = `найдено: 0 совпадений`
-		} else {
-			renderStart(result)
+			s('.search__input').placeholder = `найдено: ${result.length} совпадений`
+
+			result.forEach((e, id) => {
+				let string = `<p>${id + 1}) ${e.en} - ${e.tr ? e.tr + ' - ' : ''} ${e.ru}${e.pos ? ' (' + e.pos + ')' : ''}</p>`
+
+				s('.wrap').insertAdjacentHTML('beforeend', string);
+			})
 		}
 
+		function isFirstLetterEnglish(str) {
+			return /^[a-zA-Z]/.test(str);
+		}
+		function isFirstLetterRussian(str) {
+			return /^[а-яА-Я]/.test(str);
+		}
 
 	}
 
-	function renderStart(result) {
+	const getRender = (arr, wordsIntervalOne = 0, wordsIntervalTwo = 0) => {
 		s('.wrap').textContent = ''
-		s('.search__input').value = ''
-		s('.search__input').placeholder = `найдено: ${result.length} совпадений`
 
-		result.forEach((e, id) => {
-			let string = `<p>${id + 1}) ${e.en} - ${e.tr ? e.tr + ' - ' : ''} ${e.ru}${e.pos ? ' (' + e.pos + ')' : ''}</p>`
+		if (wordsIntervalOne != 0) {
 
-			s('.wrap').insertAdjacentHTML('beforeend', string);
-		})
-	}
-
-	function isFirstLetterEnglish(str) {
-		return /^[a-zA-Z]/.test(str);
-	}
-	function isFirstLetterRussian(str) {
-		return /^[а-яА-Я]/.test(str);
-	}
-
-}
-
-const getRender = (arr, wordsIntervalOne = 0, wordsIntervalTwo = 0) => {
-	s('.wrap').textContent = ''
-
-	if (wordsIntervalOne != 0) {
-
-		if (wordsIntervalOne > wordsIntervalTwo) {
-			alert('Ошибка в интервалах')
-			return
-		}
-
-		let i = arr.findIndex(num => num.pos > wordsIntervalOne)
-		let id = 1
-		do {
-
-			let string = `<p>${id}) ${arr[i].en} - ${arr[i].tr ? arr[i].tr + ' - ' : ''}${arr[i].ru}${arr[i].exp ? ' - ' + arr[i].exp : ''}${arr[i].pos ? ' (' + arr[i].pos + ')' : ''}</p>`
-
-			if ((id + 1) % 20 === 0) {
-
-				string += '<hr class="divider">'
+			if (wordsIntervalOne > wordsIntervalTwo) {
+				alert('Ошибка в интервалах')
+				return
 			}
 
-			s('.wrap').insertAdjacentHTML('beforeend', string);
-			if (i >= arr.length - 1) {
-				break
-			}
-			id++
-			i++
+			let i = arr.findIndex(num => num.pos > wordsIntervalOne)
+			let id = 1
+			do {
 
-		} while (arr[i].pos >= wordsIntervalOne && arr[i].pos < wordsIntervalTwo)
+				let string = `<p>${id}) ${arr[i].en} - ${arr[i].tr ? arr[i].tr + ' - ' : ''}${arr[i].ru}${arr[i].exp ? ' - ' + arr[i].exp : ''}${arr[i].pos ? ' (' + arr[i].pos + ')' : ''}</p>`
 
-	} else {
-		arr.forEach((e, id) => {
-			let string = `<p>${id + 1}) ${e.en} - ${e.tr ? e.tr + ' - ' : ''}${e.ru}${e.exp ? ' - ' + e.exp : ''}${e.pos ? ' (' + e.pos + ')' : ''}</p>`
+				if ((id + 1) % 20 === 0) {
 
-			if ((id + 1) % 20 === 0) {
+					string += '<hr class="divider">'
+				}
 
-				string += '<hr class="divider">'
-			}
+				s('.wrap').insertAdjacentHTML('beforeend', string);
+				if (i >= arr.length - 1) {
+					break
+				}
+				id++
+				i++
 
-			s('.wrap').insertAdjacentHTML('beforeend', string);
-		})
-	}
+			} while (arr[i].pos >= wordsIntervalOne && arr[i].pos < wordsIntervalTwo)
 
-
-
-}
-
-getSearch()
-
-all('.nav__link').forEach(e => {
-
-	e.addEventListener('click', (evt) => {
-		evt.preventDefault()
-
-		if (ALL[e.dataset.id]) {
-			getRender(ALL[e.dataset.id])
 		} else {
+			arr.forEach((e, id) => {
+				let string = `<p>${id + 1}) ${e.en} - ${e.tr ? e.tr + ' - ' : ''}${e.ru}${e.exp ? ' - ' + e.exp : ''}${e.pos ? ' (' + e.pos + ')' : ''}</p>`
 
-			switch (e.dataset.id) {
-				case 'w-1': {
-					getRender(ALL.w, 1, 1000)
-				} break
-				case 'w-2': {
-					getRender(ALL.w, 1000, 2000)
-				} break
-				case 'w-3': {
-					getRender(ALL.w, 2000, 3000)
-				} break
-				case 'w-4': {
-					getRender(ALL.w, 3000, 4000)
-				} break
-				case 'w-5': {
-					getRender(ALL.w, 4000, 5000)
-				} break
-				case 'w-6': {
-					getRender(ALL.w, 5000, 6000)
-				} break
-				case 'w-7': {
-					getRender(ALL.w, 6000, 10000)
-				} break
-				case 'w-8': {
-					getRender(ALL.w, 10000, 22000)
-				} break
-				case 'w-9': {
-					getRender(ALL.w, 21999, 22001)
-				} break
-			}
+				if ((id + 1) % 20 === 0) {
 
+					string += '<hr class="divider">'
+				}
 
+				s('.wrap').insertAdjacentHTML('beforeend', string);
+			})
 		}
+
+
+
+	}
+
+	getSearch()
+
+	all('.nav__link').forEach(e => {
+
+		e.addEventListener('click', (evt) => {
+			evt.preventDefault()
+
+			if (ALL[e.dataset.id]) {
+				getRender(ALL[e.dataset.id])
+			} else {
+
+				switch (e.dataset.id) {
+					case 'w-1': {
+						getRender(ALL.w, 1, 1000)
+					} break
+					case 'w-2': {
+						getRender(ALL.w, 1000, 2000)
+					} break
+					case 'w-3': {
+						getRender(ALL.w, 2000, 3000)
+					} break
+					case 'w-4': {
+						getRender(ALL.w, 3000, 4000)
+					} break
+					case 'w-5': {
+						getRender(ALL.w, 4000, 5000)
+					} break
+					case 'w-6': {
+						getRender(ALL.w, 5000, 6000)
+					} break
+					case 'w-7': {
+						getRender(ALL.w, 6000, 10000)
+					} break
+					case 'w-8': {
+						getRender(ALL.w, 10000, 22000)
+					} break
+					case 'w-9': {
+						getRender(ALL.w, 21999, 22001)
+					} break
+				}
+
+
+			}
+		})
+
+
+
+
 	})
 
+	getNum()
 
 
+}
 
-})
-
-getNum()
-
-
-
+startScript()
