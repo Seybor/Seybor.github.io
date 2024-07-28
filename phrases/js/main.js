2cea579ef89c85aa85e90c08362175f43ed0e664
Seybor <sashaborisenko1996@gmail.com>
2024-07-28 10:11:24 +0700

    1

--- phrases/js/main.js
+++ phrases/js/main.js
@@ -18,8 +18,15 @@ $.each($('.header-item'), function (id, el) {
 
 		ALL_PH[$(this).attr('data-type')].forEach((el, id) => {
 			let str = `<p data-id="${id + 1}">`
-			el.forEach(els => {
-				str += `<span>${els}</span>`
+			el.forEach((els, id) => {
+
+				if (id > 1) {
+					str += `<span style='font-size:0.75rem;'>${els}</span>`
+				} else {
+					str += `<span style='font-size:1rem;'>${els}</span>`
+				}
+
+
 			})
 			str += '</p>'
 
