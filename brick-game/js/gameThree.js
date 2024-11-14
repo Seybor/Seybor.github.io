ed64de7ad0af9f16136f0bd32f18376684ffdf7e
Aleksandr <115714971+Seybor@users.noreply.github.com>
2024-11-14 15:10:48 +0700

    8

--- brick-game/js/gameThree.js
+++ brick-game/js/gameThree.js
@@ -51,7 +51,7 @@ const threeGame = {
 		}, this.gameSpeed[gameControler.gameSpeed - 1])
 	},
 
-	gameSpeed: [500, 400, 300, 250, 200, 175, 150, 125, 100],
+	gameSpeed: [500, 450, 400, 350, 300, 275, 250, 225, 200],
 
 	elementsCubes: {
 		one: [
@@ -85,7 +85,57 @@ const threeGame = {
 	},
 
 	renderElements: function () {
+
+
 		for (let key in this.elementsCubes) {
+			let el = getRandomNumber(1, 4)
+
+			this.elementsCubes[key] = []
+			if (key === 'one') {
+				this.elementsCubes[key].push([2, 20])
+			} else if (key === 'two') {
+				this.elementsCubes[key].push([5, 20])
+			} else if (key === 'three') {
+				this.elementsCubes[key].push([8, 20])
+			}
+
+			if (el >= 2) {
+
+				if (key === 'one') {
+					this.elementsCubes[key].push([2, 19])
+				} else if (key === 'two') {
+					this.elementsCubes[key].push([5, 19])
+				} else if (key === 'three') {
+					this.elementsCubes[key].push([8, 19])
+				}
+
+				if (el >= 3) {
+
+					if (key === 'one') {
+						this.elementsCubes[key].push([3, 20])
+					} else if (key === 'two') {
+						this.elementsCubes[key].push([6, 20])
+					} else if (key === 'three') {
+						this.elementsCubes[key].push([9, 20])
+					}
+
+					if (el === 4) {
+
+						if (key === 'one') {
+							this.elementsCubes[key].push([3, 19])
+						} else if (key === 'two') {
+							this.elementsCubes[key].push([6, 19])
+						} else if (key === 'three') {
+							this.elementsCubes[key].push([9, 19])
+						}
+
+
+					}
+
+				}
+			}
+
+
 			for (let i = 0; i < this.elementsCubes[key].length; i++) {
 				let x = this.elementsCubes[key][i][0]
 				let y = this.elementsCubes[key][i][1]
@@ -99,6 +149,54 @@ const threeGame = {
 	renderEnemy: function () {
 
 		for (let key in this.enemyCubes) {
+
+			let el = getRandomNumber(1, 4)
+
+			this.enemyCubes[key] = []
+			if (key === 'one') {
+				this.enemyCubes[key].push([2, 2])
+			} else if (key === 'two') {
+				this.enemyCubes[key].push([5, 2])
+			} else if (key === 'three') {
+				this.enemyCubes[key].push([8, 2])
+			}
+
+			if (el >= 2) {
+
+				if (key === 'one') {
+					this.enemyCubes[key].push([2, 1])
+				} else if (key === 'two') {
+					this.enemyCubes[key].push([5, 1])
+				} else if (key === 'three') {
+					this.enemyCubes[key].push([8, 1])
+				}
+
+				if (el >= 3) {
+
+					if (key === 'one') {
+						this.enemyCubes[key].push([3, 2])
+					} else if (key === 'two') {
+						this.enemyCubes[key].push([6, 2])
+					} else if (key === 'three') {
+						this.enemyCubes[key].push([9, 2])
+					}
+
+					if (el === 4) {
+
+						if (key === 'one') {
+							this.enemyCubes[key].push([3, 1])
+						} else if (key === 'two') {
+							this.enemyCubes[key].push([6, 1])
+						} else if (key === 'three') {
+							this.enemyCubes[key].push([9, 1])
+						}
+
+
+					}
+
+				}
+			}
+
 			for (let i = 0; i < this.enemyCubes[key].length; i++) {
 				let x = this.enemyCubes[key][i][0]
 				let y = this.enemyCubes[key][i][1]
@@ -133,7 +231,14 @@ const threeGame = {
 		}
 
 		this.clearCube(cube)
-		this.renderElements(cube)
+
+		for (let i = 0; i < this.elementsCubes[cube].length; i++) {
+			let x = this.elementsCubes[cube][i][0]
+			let y = this.elementsCubes[cube][i][1]
+
+			s('.content-display__field div[data-coord="' + x + ',' + y + '"]').classList.add('block')
+			attr('.content-display__field div[data-coord="' + x + ',' + y + '"]', { 'data-cube': `${cube}` })
+		}
 
 	},
 
@@ -180,7 +285,16 @@ const threeGame = {
 			return
 		}
 
-		this.renderEnemy()
+		for (let key in this.enemyCubes) {
+			for (let i = 0; i < this.enemyCubes[key].length; i++) {
+				let x = this.enemyCubes[key][i][0]
+				let y = this.enemyCubes[key][i][1]
+
+				s('.content-display__field div[data-coord="' + x + ',' + y + '"]').classList.add('block-enemy')
+				attr('.content-display__field div[data-coord="' + x + ',' + y + '"]', { 'data-cube': 'enemy' })
+			}
+		}
+
 
 	},
 
