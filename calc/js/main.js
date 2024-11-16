e5f7db046c401dd5ca7d4596f77a75c9f7b5fdb7
Aleksandr <115714971+Seybor@users.noreply.github.com>
2024-11-16 10:37:40 +0700

    calc

--- calc/js/main.js
+++ calc/js/main.js
@@ -1,376 +1,71 @@
 import { s, all, css, attr, html, text, insert, evt } from './modules/base.js'
 
-const calcControl = {
-	init: function () {
+function startCalc() {
 
-		this.result = 0
-		this.numberOne = ''
-		this.numberTwo = ''
-		this.isOn = false
-		this.act = ''
-		this.equal = false
-		this.setControls()
-	},
+	let result = 0
+	let addNumber = 0
+	let isOn = false
+	let act = ''
+	let equal = false
+	let setZeroAddNumber = false
 
-	result: 0,
-	addNumber: 0,
-	isOn: false,
-	act: '',
-	equal: false,
-	setZeroAddNumber: false,
+	if (localStorage.getItem('calc')) {
+		isOn = true
+		result = localStorage.getItem('calc')
+		updateNumber(result)
+	}
 
-	add: function (a, b) {
-		this.result = +a + +b
 
-		this.updateNumber(this.result)
-	},
 
-	sub: function (a, b) {
-		this.result = a - b
-		this.updateNumber(this.result)
-	},
 
-	factorial: function (a) {
+	function add(a, b) {
+		result = +a + +b
+		localStorage.setItem('calc', result)
+		updateNumber(result)
+	}
+
+	function sub(a, b) {
+		result = a - b
+		localStorage.setItem('calc', result)
+		updateNumber(result)
+	}
+
+	function factorial(a) {
 		if (a > 10) {
 			text('.calc__display', '>99999999')
 			return
 		}
 		for (let i = 1; i <= a; i++) {
-			this.result *= i
+			result *= i
 		}
-		this.updateNumber(this.result)
-	},
+		localStorage.setItem('calc', result)
+		updateNumber(result)
+	}
 
-	sqrt: function (a) {
-		this.result = Math.sqrt(a).toFixed(2)
-		this.updateNumber(this.result)
-	},
+	function sqrt(a) {
+		result = Math.sqrt(a).toFixed(2)
+		localStorage.setItem('calc', result)
+		updateNumber(result)
+	}
 
-	multiplication: function (a, b) {
-		this.result = a * b
-		this.updateNumber(this.result)
-	},
+	function multiplication(a, b) {
+		result = a * b
+		localStorage.setItem('calc', result)
+		updateNumber(result)
+	}
 
-	devide: function (a, b) {
+	function devide(a, b) {
 
 		if (b == 0) {
 			text('.calc__display', 'error')
 			return
 		}
-		this.result = (a / b).toFixed(2)
-		this.updateNumber(this.result)
-	},
-
-	setControls: function () {
-
-		all('.calc-control__keys').forEach((el, id) => {
-
-			el.addEventListener('click', (evt) => {
-
-				if (this.isOn) {
-
-					if (evt.target.dataset.control - evt.target.dataset.control === 0) {
-
-						if (this.result === 0) {
-
-							this.result = evt.target.dataset.control
-							this.updateNumber(this.result)
-						} else {
-
-							if (this.addNumber === 0) {
-								this.result += evt.target.dataset.control
-								this.updateNumber(this.result)
-
-							} else {
-
-								if (this.addNumber === '0') {
-									this.addNumber = evt.target.dataset.control
-								} else {
-
-									if (this.setZeroAddNumber) {
-										this.addNumber = evt.target.dataset.control
-										this.setZeroAddNumber = false
-									} else {
-										this.addNumber += evt.target.dataset.control
-									}
-
-								}
-
-								this.updateNumber(this.addNumber)
-
-							}
-
-						}
-
-
-
-
-					}
-
-				}
-
-
-			})
-
-
-		})
-
-		evt('.calc-control__keys[data-control="="', 'click', (evt) => {
-
-			if (this.isOn) {
-
-				switch (this.act) {
-					case '+': {
-						this.add(this.result, this.addNumber)
-					} break
-					case '-': {
-						this.sub(this.result, this.addNumber)
-					} break
-					case '!': {
-						this.factorial(this.result)
-					} break
-					case '√': {
-						this.sqrt(this.result)
-					} break
-					case '*': {
-						this.multiplication(this.result, this.addNumber)
-					} break
-					case '/': {
-						this.devide(this.result, this.addNumber)
-					} break
-				}
-
-
-				this.setZeroAddNumber = true
-				this.equal = true
-
-			}
-
-
-		})
-
-
-		evt('.calc-control__keys[data-control="+"]', 'click', (evt) => {
-
-
-			if (this.isOn) {
-
-				if (this.result === 0) {
-
-					return
-
-				} else {
-
-					this.act = '+'
-
-					if (this.addNumber === 0) {
-						this.addNumber = '0'
-						return
-					} else {
-
-						if (this.equal) {
-							return
-						} else {
-							this.add(this.result, this.addNumber)
-							this.equal = true
-						}
-
-
-					}
-
-				}
-
-			}
-
-
-		})
-
-		evt('.calc-control__keys[data-control="-"]', 'click', (evt) => {
-
-
-			if (this.isOn) {
-
-				if (this.result === 0) {
-
-					return
-
-				} else {
-
-					this.act = '-'
-
-					if (this.addNumber === 0) {
-						this.addNumber = '0'
-						return
-					} else {
-
-						if (this.equal) {
-							return
-						} else {
-							this.sub(this.result, this.addNumber)
-							this.equal = true
-						}
-
-
-					}
-
-
-
-				}
-
-			}
-
-
-		})
-
-		evt('.calc-control__keys[data-control="*"]', 'click', (evt) => {
-
-
-			if (this.isOn) {
-
-				if (this.result === 0) {
-
-					return
-
-				} else {
-
-					this.act = '*'
-
-					if (this.addNumber === 0) {
-						this.addNumber = '0'
-						return
-					} else {
-
-						if (this.equal) {
-							return
-						} else {
-							this.multiplication(this.result, this.addNumber)
-							this.equal = true
-						}
-
-
-					}
-
-
-
-				}
-
-			}
-
-
-		})
-
-		evt('.calc-control__keys[data-control="/"]', 'click', (evt) => {
-
-
-			if (this.isOn) {
-
-				if (this.result === 0) {
-
-					return
-
-				} else {
-
-					this.act = '/'
-
-					if (this.addNumber === 0) {
-						this.addNumber = '0'
-						return
-					} else {
-
-						if (this.equal) {
-							return
-						} else {
-							this.devide(this.result, this.addNumber)
-							this.equal = true
-						}
-
-
-					}
-
-
-
-				}
-
-			}
-
-
-		})
-
-		evt('.calc-control__keys[data-control="!"]', 'click', (evt) => {
-
-
-			if (this.isOn) {
-
-				if (this.result === 0) {
-
-					return
-
-				} else {
-
-					this.act = '!'
-
-					if (this.equal) {
-						return
-					} else {
-						this.factorial(this.result)
-						this.equal = true
-					}
-				}
-
-			}
-
-
-		})
-
-		evt('.calc-control__keys[data-control="√"]', 'click', (evt) => {
-
-
-			if (this.isOn) {
-
-				if (this.result === 0) {
-
-					return
-
-				} else {
-
-					this.act = '√'
-
-					if (this.equal) {
-						return
-					} else {
-						this.sqrt(this.result)
-						this.equal = true
-					}
-				}
-
-			}
-
-
-		})
-
-		evt('.calc-control__keys[data-control="off"]', 'click', (evt) => {
-
-			this.isOn = false
-			this.result = 0
-			this.addNumber = 0
-			this.act = ''
-			this.equal = false
-			this.updateNumber('')
-
-		})
-
-		evt('.calc-control__keys[data-control="on/c"]', 'click', (evt) => {
-			this.isOn = true
-			this.result = 0
-			this.addNumber = 0
-			this.act = ''
-			this.equal = false
-			this.updateNumber(this.result)
-		})
-
-	},
-
-	updateNumber: function (value) {
+		result = (a / b).toFixed(2)
+		localStorage.setItem('calc', result)
+		updateNumber(result)
+	}
+
+	function updateNumber(value) {
 
 		if (+value > 99999999) {
 			text('.calc__display', '>99999999')
@@ -380,8 +75,322 @@ const calcControl = {
 
 		}
 
-
 	}
+
+
+
+	all('.calc-control__keys').forEach((el, id) => {
+
+		el.addEventListener('click', (evt) => {
+
+			if (isOn) {
+
+				if (evt.target.dataset.control - evt.target.dataset.control === 0) {
+
+					if (result === 0) {
+
+						result = evt.target.dataset.control
+						updateNumber(result)
+					} else {
+
+						if (addNumber === 0) {
+							result += evt.target.dataset.control
+							updateNumber(result)
+
+						} else {
+
+							if (addNumber === '0') {
+								addNumber = evt.target.dataset.control
+							} else {
+
+								if (setZeroAddNumber) {
+									addNumber = evt.target.dataset.control
+									setZeroAddNumber = false
+								} else {
+									addNumber += evt.target.dataset.control
+								}
+
+							}
+
+							updateNumber(addNumber)
+
+						}
+
+					}
+
+
+
+
+				}
+
+			}
+
+
+		})
+
+
+	})
+
+	evt('.calc-control__keys[data-control="="', 'click', (evt) => {
+
+		if (isOn) {
+
+			switch (act) {
+				case '+': {
+					add(result, addNumber)
+				} break
+				case '-': {
+					sub(result, addNumber)
+				} break
+				case '!': {
+					factorial(result)
+				} break
+				case '√': {
+					sqrt(result)
+				} break
+				case '*': {
+					multiplication(result, addNumber)
+				} break
+				case '/': {
+					devide(result, addNumber)
+				} break
+			}
+
+
+			setZeroAddNumber = true
+			equal = true
+
+		}
+
+
+	})
+
+
+	evt('.calc-control__keys[data-control="+"]', 'click', (evt) => {
+
+
+		if (isOn) {
+
+			if (result === 0) {
+
+				return
+
+			} else {
+
+				act = '+'
+
+				if (addNumber === 0) {
+					addNumber = '0'
+					return
+				} else {
+
+					if (equal) {
+						return
+					} else {
+						add(result, addNumber)
+						equal = true
+					}
+
+
+				}
+
+			}
+
+		}
+
+
+	})
+
+	evt('.calc-control__keys[data-control="-"]', 'click', (evt) => {
+
+
+		if (isOn) {
+
+			if (result === 0) {
+
+				return
+
+			} else {
+
+				act = '-'
+
+				if (addNumber === 0) {
+					addNumber = '0'
+					return
+				} else {
+
+					if (equal) {
+						return
+					} else {
+						sub(result, addNumber)
+						equal = true
+					}
+
+
+				}
+
+
+
+			}
+
+		}
+
+
+	})
+
+	evt('.calc-control__keys[data-control="*"]', 'click', (evt) => {
+
+
+		if (isOn) {
+
+			if (result === 0) {
+
+				return
+
+			} else {
+
+				act = '*'
+
+				if (addNumber === 0) {
+					addNumber = '0'
+					return
+				} else {
+
+					if (equal) {
+						return
+					} else {
+						multiplication(result, addNumber)
+						equal = true
+					}
+
+
+				}
+
+
+
+			}
+
+		}
+
+
+	})
+
+	evt('.calc-control__keys[data-control="/"]', 'click', (evt) => {
+
+
+		if (isOn) {
+
+			if (result === 0) {
+
+				return
+
+			} else {
+
+				act = '/'
+
+				if (addNumber === 0) {
+					addNumber = '0'
+					return
+				} else {
+
+					if (equal) {
+						return
+					} else {
+						devide(result, addNumber)
+						equal = true
+					}
+
+
+				}
+
+
+
+			}
+
+		}
+
+
+	})
+
+	evt('.calc-control__keys[data-control="!"]', 'click', (evt) => {
+
+
+		if (isOn) {
+
+			if (result === 0) {
+
+				return
+
+			} else {
+
+				act = '!'
+
+				if (equal) {
+					return
+				} else {
+					factorial(result)
+					equal = true
+				}
+			}
+
+		}
+
+
+	})
+
+	evt('.calc-control__keys[data-control="√"]', 'click', (evt) => {
+
+
+		if (isOn) {
+
+			if (result === 0) {
+
+				return
+
+			} else {
+
+				act = '√'
+
+				if (equal) {
+					return
+				} else {
+					sqrt(result)
+					equal = true
+				}
+			}
+
+		}
+
+
+	})
+
+	evt('.calc-control__keys[data-control="off"]', 'click', (evt) => {
+
+		isOn = false
+		result = 0
+		addNumber = 0
+		act = ''
+		equal = false
+		updateNumber('')
+		localStorage.clear()
+
+	})
+
+	evt('.calc-control__keys[data-control="on/c"]', 'click', (evt) => {
+		isOn = true
+		result = 0
+		addNumber = 0
+		act = ''
+		equal = false
+		updateNumber(result)
+		localStorage.clear()
+	})
+
 }
 
-calcControl.init()
+
+startCalc()
+
+
+
+
